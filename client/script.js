c791daa84d488297f63da077fe1d1864139b9143
Akshayus27 <akshayus27@gmail.com>
2023-02-03 00:43:56 +0530

    updated production server endpoint

--- client/script.js
+++ client/script.js
@@ -72,7 +72,7 @@ const handleSubmit = async (e) => {
 
   loader(msgDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch(import.meta.env.VITE_SERVER_ENDPOINT || 'http://localhost:5000', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
